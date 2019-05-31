cb4337f92e92abcfd3a97af27b53b7ff5658ff5a
Elnura Donabedian <elnurab@gmail.com>
2019-05-31 23:23:40 +0200

    mycropython

--- flexible_web_server/main.py
+++ flexible_web_server/main.py
@@ -1,3 +1,6 @@
+import machine
+from machine import Pin
+
 try:
     import usocket as socket
 except:
@@ -42,9 +45,38 @@ def dummy():
 
     return response_template % body
 
+pin = machine.Pin(9, machine.Pin.OUT)
+
+def light_on():
+    pin.value(1)
+    body = "You turned a light on!"
+    return response_template % body
+
+def light_off():
+    pin.value(0)
+    body = "You turned a light off!"
+    return response_template % body
+
+
+switch_pin = machine.Pin(10, machine.Pin.IN)
+def switch():
+    body = "{state: " + str(switch_pin.value()) + "}"
+    return response_template % body
+
+adc = machine.ADC(0)
+
+def light():
+    body = "{value: " + str(adc.read()) + "}"
+    return response_template % body
+
+
 handlers = {
     'time': time,
     'dummy': dummy,
+    'light_on': light_on,
+    'light_off': light_off,
+    'switch': switch,
+    'light': light,
 }
 
 
